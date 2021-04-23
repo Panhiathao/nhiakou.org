9e52e5a8345b442db2b89ba4bb7a8629a3130114
Thon Ly <thonly@heartbank.ceo>
2021-04-22 18:54:45 -0700

    created updateLanguage listener

--- hmong.js
+++ hmong.js
@@ -1,1 +1,5 @@
-console.log("Nyob zoo ntiaj teb")
+// console.log("Nyob zoo ntiaj teb")
+
+function updateLanguage(event) {
+    document.location = event.target.value
+}
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+// console.log("hello world")
+
+function updateLanguage(event) {
+    //console.log(event.target.value)
+    // url: hmong.html
+    // document.location = event.target.value + ".html"
+    document.location = event.target.value
+}
